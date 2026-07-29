7689c5be59f302f865014e5cb000d627750bbe0f
davidnogueiradossantos951-gif <davidnogueiradossantos951@gmail.com>
2026-07-29 13:11:53 -0300

    Update script.js

--- script.js
+++ script.js
@@ -29,3 +29,32 @@ btn.addEventListener("click", () => {
     document.getElementById("constelacao").style.display = "block";
 
 });
+const mensagens = [
+"Entre tantas pessoas no mundo, foi você quem chamou minha atenção. ❤️",
+
+"Você transformou momentos simples em lembranças que eu nunca quero esquecer. ✨",
+
+"Com você, eu encontrei um lugar onde posso ser completamente eu. 🌙",
+
+"Quando penso no amanhã, é impossível não imaginar você nele. ⭐",
+
+"E foi aí que eu percebi... você não era apenas mais uma estrela. ❤️"
+];
+
+const estrelas = document.querySelectorAll(".estrela");
+const mensagem = document.getElementById("mensagem");
+
+estrelas.forEach((estrela, index) => {
+
+    estrela.addEventListener("click", () => {
+
+        mensagem.innerHTML = mensagens[index];
+
+        estrela.style.background = "#ffd700";
+
+        estrela.style.boxShadow =
+        "0 0 20px #ffd700, 0 0 40px #ffd700";
+
+    });
+
+});
