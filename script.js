773a151ea7e83cad58d83c5870ed3c89d9352f38
davidnogueiradossantos951-gif <davidnogueiradossantos951@gmail.com>
2026-07-29 12:18:48 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
+const stars = document.querySelector(".stars");
 
+for(let i = 0; i < 150; i++){
+
+    let star = document.createElement("div");
+
+    star.classList.add("star");
+
+    star.style.left = Math.random()*100 + "%";
+    star.style.top = Math.random()*100 + "%";
+
+    star.style.animationDelay =
+    Math.random()*2 + "s";
+
+    stars.appendChild(star);
+}
